116df3432b825702db5aa652c714c403eb30cb97
Ståle Eikebråten <eiksta+gh2@verji.com>
2026-05-04 23:38:00 +0200

    Database outlined framework

---
+++ DatabaseLayer/DatabaseLayer.cs
@@ -0,0 +1,17 @@
+﻿using WebApiAbstractions.Dto;
+
+namespace DatabaseLayer
+{
+    public class DatabaseLayer : IDatabaseLayer
+    {
+        public DocumentDto GetDocument(string documentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<DocumentDto> GetDocuments(string companyId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DatabaseLayer/IDatabaseLayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiAbstractions.Dto;
+
+namespace DatabaseLayer
+{
+    internal interface IDatabaseLayer
+    {
+        public DocumentDto GetDocument(string documentId);
+        public IList<DocumentDto> GetDocuments(string companyId);
+        //public void DeleteDocuments(string[] documentIds);
+
+    }
+}
