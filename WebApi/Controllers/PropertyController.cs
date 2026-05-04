6c7353b0d92063031b4c5ebd2f26e03f1ca322bb
Ståle Eikebråten <eiksta+gh2@verji.com>
2026-05-04 23:46:13 +0200

    Add files via upload

---
+++ WebApi/Controllers/PropertyController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/properties/")]
+    [ApiController]
+    public class PopertiesController : ControllerBase
+    {
+
+        public PopertiesController(
+            ) { }
+
+        [HttpGet("{propertyid}/documents/expiring")]
+        public class GetDocument(string documentId) { 
+        }
+
+    }
+}
