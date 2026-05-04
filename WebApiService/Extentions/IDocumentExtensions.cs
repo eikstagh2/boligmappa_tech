0905bd882a78d4baaa8f2d71ca298a637567aa6b
Ståle Eikebråten <eiksta+gh2@verji.com>
2026-05-04 23:39:41 +0200

    Start framwork businesslayer

---
+++ WebApiService/Extentions/IDocumentExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiAbstractions.Dto;
+
+namespace WebApiService.Extentions
+{
+    internal interface IDocumentExtensions
+    {
+        Task<string> GetDocument(DocumentDto dto);
+        Task<IList<DocumentDto>> GetDocuments(string companyId)
+        //Task<string> DeleteRetentionPolicyForTenant(string policyId, string tenantId);
+    }
+}
